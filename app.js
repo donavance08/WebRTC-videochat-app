51ce09f5174240a84c8eaf46a33dab6ae159d633
donavance08 <adonisavance@gmail.com>
2023-02-07 05:51:59 +0800

    implement skeleton socketIo connection both in server and client

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const http = require('http');
-
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.static('./public'));
 
@@ -14,4 +14,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+/* 
+    Make socket listen for connection event and inform the user
+    print the socket.id afterwards
+*/
+io.on('connection', (socket) => {
+    console.log('user connected to socket.IO server');
+    console.group(socket.id);
+})
+
 app.listen(PORT, () => console.log(`Server is online on port ${PORT}`));
+
+
